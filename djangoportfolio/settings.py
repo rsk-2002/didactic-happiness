9c465ace04de69f47cd590a3e8d6257119d247e3
Ravi Shanker Kushwaha <ravishankerk48@gmail.com>
2022-04-30 15:48:23 +0530

    change in settings.py and deploy

--- djangoportfolio/settings.py
+++ djangoportfolio/settings.py
@@ -130,9 +130,9 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
 
-MEDIA_URL = 'images/'
+MEDIA_URL = '/images/'
 
 
 # Default primary key field type
