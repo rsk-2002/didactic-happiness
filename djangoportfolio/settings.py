8f7c43af277821682b5ea149d3f144b71a1af1ef
Ravi Shanker Kushwaha <ravishankerk48@gmail.com>
2022-04-30 16:01:34 +0530

    changes in settings.py

--- djangoportfolio/settings.py
+++ djangoportfolio/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images/')
 
 MEDIA_URL = '/images/'
 
