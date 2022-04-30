7f8a33d43b3d0b1566420d25e469e0bef87746ef
Ravi Shanker Kushwaha <ravishankerk48@gmail.com>
2022-04-30 15:36:30 +0530

    change in static urls and urls.py

--- djangoportfolio/urls.py
+++ djangoportfolio/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('inbox/', views.inboxPage, name="inbox"),
     path('inbox/<str:pk>/', views.inboxMessage, name="inbox-message"),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
