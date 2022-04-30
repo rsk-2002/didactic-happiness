4d5cf75bab085f9869ed1343630ad21581467411
Ravi Shanker Kushwaha <ravishankerk48@gmail.com>
2022-04-30 15:21:57 +0530

    change in settings.py

--- djangoportfolio/settings.py
+++ djangoportfolio/settings.py
@@ -139,3 +139,4 @@ MEDIA_URL = 'uploads/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["https://rsk2022.herokuapp.com"]
